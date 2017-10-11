eedb1d248b39fe3dd7a7f18bbe8332e1524e9249
spascual <santi.pdp@gmail.com>
2017-10-11 13:45:54 +0200

    refactor: train_aco w/ slurm

--- train_aco.sh
+++ train_aco.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 
+SAVE_PATH="aco_experiments"
+BATCH_SIZE=50
+SEQ_LEN=50
+MEM=180000
+
 # MO acoustic
-python main.py --train-aco --save_path test_mo --exclude_train_spks 75 \
-     	       --aco_max_seq_len 35 --exclude_eval_spks 75 \
-               --batch_size 15 --aco_mulout --force-gen --cuda
+srun -p veu --mem $MEM --gres=gpu:1 python main.py --train-aco --save_path "$SAVE_PATH/"mo_aco_5spk \
+						    --exclude_train_spks 75 \
+						    --aco_max_seq_len $SEQ_LEN --exclude_eval_spks 75 \
+						    --batch_size $BATCH_SIZE --aco_mulout --force-gen --cuda | tee "$SAVE_PATH"/mo_aco_5spk.log &
 
 # Train each speaker 72 and 73
 
-python main.py --train-aco --save_path so_aco_1spk-72 --batch_size 15 \
-	--cuda --aco_max_seq_len 35 --batch_size 15 --cfg cfg/tcstar_72.cfg \
-	--aco_train_forced_trim 17879 --aco_valid_forced_trim 2294
+# 72
+#srun -p veu --mem $MEM --gres=gpu:1 python main.py --train-aco --save_path "$SAVE_PATH/"so_aco_1spk-72 \
+#						    --aco_max_seq_len $SEQ_LEN --batch_size $BATCH_SIZE \
+#						    --cuda --cfg cfg/tcstar_73.cfg \
+#						    --aco_train_forced_trim 17879 --aco_valid_forced_trim 2294 | tee "$SAVE_PATH"/so_aco_1spk-72.log  &
 
 
-python main.py --train-aco --save_path so_aco_1spk-73 --batch_size 15 \
-	--cuda --aco_max_seq_len 35 --batch_size 15 --cfg cfg/tcstar_73.cfg \
-	--aco_train_forced_trim 17879 --aco_valid_forced_trim 2294
+# 73
+#srun -p veu --mem $MEM --gres=gpu:1 python main.py --train-aco --save_path "$SAVE_PATH/"so_aco_1spk-73 \
+#						    --aco_max_seq_len $SEQ_LEN --batch_size $BATCH_SIZE \
+#						    --cuda --cfg cfg/tcstar_72.cfg \
+#						    --aco_train_forced_trim 17879 --aco_valid_forced_trim 2294 | tee "$SAVE_PATH"/so_aco_1spk-72.log  &
 
-	
 
 # SO acoustic
-#python main.py --train-aco --save_path so_aco_5spk --exclude_train_spks 75 \
-#	      --aco_max_seq_len 35 --exclude_eval_spks 75 \
-#              --batch_size 15  
+srun -p veu --mem $MEM --gres=gpu:1 python main.py --train-aco --save_path "$SAVE_PATH/"so_aco_5spk --exclude_train_spks 75 \
+						    --aco_max_seq_len $SEQ_LEN --exclude_eval_spks 75 \
+						    --batch_size $BATCH_SIZE --cuda | tee "$SAVE_PATH"/so_aco_5spk.log  &
