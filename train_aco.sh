975e7b05351fbe99edecd9afb9230ea60b6c5cbd
Santi DSP <santi.pdp@gmail.com>
2018-02-26 12:07:31 +0100

    fix: tran aco script

--- train_aco.sh
+++ train_aco.sh
@@ -3,31 +3,19 @@
 SAVE_PATH="aco_experiments"
 BATCH_SIZE=50
 SEQ_LEN=50
-MEM=180000
 
-# MO acoustic
-srun -p veu --mem $MEM --gres=gpu:1 python main.py --train-aco --save_path "$SAVE_PATH/"mo_aco_5spk \
-						    --exclude_train_spks 75 \
-						    --aco_max_seq_len $SEQ_LEN --exclude_eval_spks 75 \
-						    --batch_size $BATCH_SIZE --aco_mulout --force-gen --cuda | tee "$SAVE_PATH"/mo_aco_5spk.log &
+# SO acoustic (73)
+python -u main.py --train-aco --save_path "$SAVE_PATH/"so_aco_73 \
+		  --aco_max_seq_len $SEQ_LEN \
+		  --batch_size $BATCH_SIZE --cuda --cfg cfg/tcstar_73.cfg
 
-# Train each speaker 72 and 73
+# MO acoustic (5 spk)
+python -u main.py --train-aco --save_path "$SAVE_PATH/"mo_aco_5spk \
+		  --aco_max_seq_len $SEQ_LEN \
+		  --batch_size $BATCH_SIZE --cuda --cfg cfg/tcstar.cfg \
+		  --exclude_train_spks 75 --exclude_eval_spks 75 --aco_mulout
 
-# 72
-#srun -p veu --mem $MEM --gres=gpu:1 python main.py --train-aco --save_path "$SAVE_PATH/"so_aco_1spk-72 \
-#						    --aco_max_seq_len $SEQ_LEN --batch_size $BATCH_SIZE \
-#						    --cuda --cfg cfg/tcstar_73.cfg \
-#						    --aco_train_forced_trim 17879 --aco_valid_forced_trim 2294 | tee "$SAVE_PATH"/so_aco_1spk-72.log  &
-
-
-# 73
-#srun -p veu --mem $MEM --gres=gpu:1 python main.py --train-aco --save_path "$SAVE_PATH/"so_aco_1spk-73 \
-#						    --aco_max_seq_len $SEQ_LEN --batch_size $BATCH_SIZE \
-#						    --cuda --cfg cfg/tcstar_72.cfg \
-#						    --aco_train_forced_trim 17879 --aco_valid_forced_trim 2294 | tee "$SAVE_PATH"/so_aco_1spk-72.log  &
-
-
-# SO acoustic
-srun -p veu --mem $MEM --gres=gpu:1 python main.py --train-aco --save_path "$SAVE_PATH/"so_aco_5spk --exclude_train_spks 75 \
-						    --aco_max_seq_len $SEQ_LEN --exclude_eval_spks 75 \
-						    --batch_size $BATCH_SIZE --cuda | tee "$SAVE_PATH"/so_aco_5spk.log  &
+# SO acoustic (5 spk)
+python -u main.py --train-aco --save_path "$SAVE_PATH/"so_aco_5spk --exclude_train_spks 75 \
+		  --aco_max_seq_len $SEQ_LEN --exclude_eval_spks 75 \
+		  --batch_size $BATCH_SIZE --cuda 
