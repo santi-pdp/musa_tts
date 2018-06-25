111a1386997049acfaa1a1b6698eedbd97537b44
spascual <santi.pdp@gmail.com>
2018-06-25 15:43:12 +0200

    minor changes

--- musa/datasets/__init__.py
+++ musa/datasets/__init__.py
@@ -1,5 +1,7 @@
 from .tcstar import TCSTAR_dur, TCSTAR_aco
 from .tcstar import varlen_dur_collate
 from .tcstar import varlen_aco_collate
+from .vctk import VCTK
 from .sampler import MOSampler
+from .collaters import Aco2Id_Collater
 from .utils import *
